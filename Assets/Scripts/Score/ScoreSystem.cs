d545410413c99cfeb34d414cd3c3614008988b44
RangerHalt2 <141537190+RangerHalt2@users.noreply.github.com>
2026-05-20 20:07:09 -0400

    Quick Push for Ryan

---
+++ Assets/Scripts/Score/ScoreSystem.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class ScoreSystem : MonoBehaviour
+{
+    [SerializeField] private float winMargin = 0.75f; // % stuff
+
+    private Chimera chimeraRef;
+
+    private float cummulativePoints;
+
+    private void Start()
+    {
+        chimeraRef = GameObject.FindAnyObjectByType<Chimera>();
+    }
+
+
+
+    public void SubmitChimera()
+    {
+
+    }
+
+}
